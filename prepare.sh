56215ee06da758098ad51897a5878b88761b72b4
Calvin Lau <calvin@mona.co>
2019-10-22 17:43:43 +0800

    💚 Changed drone not to install dependencies

--- prepare.sh
+++ prepare.sh
@@ -5,11 +5,11 @@ if [ ! -z "${TRAVIS}" ]; then
     sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
     rm -rf /var/lib/apt/lists/*
 fi
-sudo apt update
-sudo apt install wget curl -y
-sudo apt install python3 libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev  gcc binutils-dev libc6-dev -y
-sudo apt install pkg-config -y
-sudo apt install build-essential -y
+apt update
+apt install wget curl -y
+apt install python3 libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev  gcc binutils-dev libc6-dev -y
+apt install pkg-config -y
+apt install build-essential -y
 
 command -v cmake > /dev/null
 if [ x"$?" = "x1" ]; then
@@ -23,7 +23,7 @@ if [ x"$?" = "x1" ]; then
         make install
         cd ..
     else
-        sudo apt install cmake -y
+        apt install cmake -y
     fi
 fi
 cmake --version
