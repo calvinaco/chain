49f3619e2d89822f4e1c25f00ffddcdeeec5bd19
Calvin Lau <calvin@mona.co>
2019-10-22 16:59:11 +0800

    💚 Changed CI script

--- install_zeromq.sh
+++ install_zeromq.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-FILE=/home/travis/lib/libzmq.so
-if [ ! -f "$FILE" ]; then
-    echo "$FILE does not exist"
-    wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
-    tar xvfz libsodium-1.0.16.tar.gz
-    cd libsodium-1.0.16
-    ./configure --prefix=$HOME
-    make
-    make install
-    cd ..
 
-    wget https://github.com/zeromq/libzmq/releases/download/v4.2.5/zeromq-4.2.5.tar.gz
-    tar xvfz zeromq-4.2.5.tar.gz
-    cd zeromq-4.2.5
-    ./configure --prefix=$HOME --with-libsodium
-    make
-    make install
-    cd ..
+if [ ! -z "${TRAVIS}" ]; then
+    FILE="${HOME}/lib/libzmq.so"
+    if [ ! -f "$FILE" ]; then
+        echo "$FILE does not exist"
+        wget --no-verbose https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+        tar xvfz libsodium-1.0.16.tar.gz
+        cd libsodium-1.0.16
+        ./configure --prefix=$HOME
+        make
+        make install
+        cd ..
+
+        wget --no-verbose https://github.com/zeromq/libzmq/releases/download/v4.3.2/zeromq-4.3.2.tar.gz
+        tar xvfz zeromq-4.3.2.tar.gz
+        cd zeromq-4.3.2
+        ./configure --prefix=$HOME --with-libsodium
+        make
+        make install
+        cd ..
+    fi
+else
+    apt install libzmq3-dev -y
 fi
