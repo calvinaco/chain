23252a6612e435507f94b7915ec2eccbfa382973
Calvin Lau <calvin@mona.co>
2019-10-25 19:12:44 +0800

    Show path

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-curl -sSL "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
-chmod +x docker-compose
-./docker-compose up
 rustup default nightly-2019-08-01-x86_64-unknown-linux-gnu
 
 # Build test environment
