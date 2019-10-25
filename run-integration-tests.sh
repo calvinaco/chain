29242bfd31e52f53169c3145df05e2fb8cc676f0
Calvin Lau <calvin@mona.co>
2019-10-25 18:56:12 +0800

    Added docker-compose

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+docker-compose up
 rustup default nightly-2019-08-01-x86_64-unknown-linux-gnu
 
 # Build test environment
