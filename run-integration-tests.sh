183afc3511336cb85ef8e267672e56f89eef296f
Calvin Lau <calvin@mona.co>
2019-10-25 18:57:42 +0800

    Installed docker-compose

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+chmod +x docker-compose
 docker-compose up
 rustup default nightly-2019-08-01-x86_64-unknown-linux-gnu
 
@@ -8,13 +9,13 @@ rustup default nightly-2019-08-01-x86_64-unknown-linux-gnu
 cd integration-tests
 ./prepare.sh
 . ./env.sh
-docker-compose up -d
+./docker-compose up -d
 
 sleep 25
 
 # Docker status
 docker ps
-docker-compose logs -t --tail="all"
+./docker-compose logs -t --tail="all"
 
 # Preliminary tests
 ./run-test.sh
